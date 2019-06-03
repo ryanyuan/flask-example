7c70bd9863f983a4b45b0c6f4399ae4107fdec1f
ryanyuan <ryan.yuan@outlook.com>
2019-06-03 22:34:37 +1000

    Finish homepage bootstrap
    Finish homepage bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+from config import app_config
 
 
-app = Flask(__name__, instance_relative_config=True)
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    # app.config.from_pyfile('config.py')
 
-from app import views
+    Bootstrap(app)
 
-app.config.from_object("config")
+    from app import models
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+    return app
---
+++ app/home/views.py
@@ -0,0 +1,11 @@
+from flask import abort, render_template
+
+from . import home
+
+
+@home.route('/')
+def homepage():
+    """
+    Render the homepage template on the / route
+    """
+    return render_template('home/index.html', title="Welcome")
